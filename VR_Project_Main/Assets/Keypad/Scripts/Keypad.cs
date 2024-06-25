43a9685bb8f30ae5dc486438e7c0322b703ffd9b
ValeraZSD <valerazsd@gmail.com>
2024-06-25 15:54:58 +0200

    Keypad update

--- VR_Project_Main/Assets/Keypad/Scripts/Keypad.cs
+++ VR_Project_Main/Assets/Keypad/Scripts/Keypad.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
